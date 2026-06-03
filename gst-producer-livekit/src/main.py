1d07e1ce7e8360eb403138fef80a02b266eb9c3e
Daniel Koch <daniel@dkprog.com>
2026-06-03 21:14:20 +0000

    Send RGBA video

--- gst-producer-livekit/src/main.py
+++ gst-producer-livekit/src/main.py
@@ -27,10 +27,7 @@ class ProducerPipeline:
     src: Gst.Element
     caps_src: Gst.Element
     videoconvert: Gst.Element
-    queue: Gst.Element
-    vp8enc: Gst.Element
-    rtppay: Gst.Element
-    caps_rtp: Gst.Element
+    caps_rgba: Gst.Element
     sink: Gst.Element
 
     def __init__(self, loop: GLib.MainLoop, video_source: rtc.VideoSource):
@@ -58,27 +55,12 @@ class ProducerPipeline:
         self.videoconvert = Gst.ElementFactory.make("videoconvert", "videoconvert")
         self.pipeline.add(self.videoconvert)
 
-        self.queue = Gst.ElementFactory.make("queue", "queue")
-        self.pipeline.add(self.queue)
-
-        self.vp8enc = Gst.ElementFactory.make("vp8enc", "vp8enc")
-        self.vp8enc.set_property("deadline", 1)
-        self.vp8enc.set_property("cpu-used", 4)
-        self.vp8enc.set_property("error-resilient", 1)
-        self.vp8enc.set_property("keyframe-max-dist", 30)
-        self.vp8enc.set_property("target-bitrate", 1000000)
-        self.pipeline.add(self.vp8enc)
-
-        self.rtppay = Gst.ElementFactory.make("rtpvp8pay", "rtppay")
-        self.rtppay.set_property("pt", 96)
-        self.pipeline.add(self.rtppay)
-
-        self.caps_rtp = Gst.ElementFactory.make("capsfilter", "caps_rtp")
+        self.caps_rgba = Gst.ElementFactory.make("capsfilter", "caps_rgba")
         caps = Gst.Caps.from_string(
-            "application/x-rtp,media=video,encoding-name=VP8,payload=96,clock-rate=90000"
+            f"video/x-raw,format=RGBA,width={FRAME_WIDTH},height={FRAME_HEIGHT},framerate=30/1"
         )
-        self.caps_rtp.set_property("caps", caps)
-        self.pipeline.add(self.caps_rtp)
+        self.caps_rgba.set_property("caps", caps)
+        self.pipeline.add(self.caps_rgba)
 
         self.sink = Gst.ElementFactory.make("appsink", "sink")
         self.sink.set_property("emit-signals", True)
@@ -90,11 +72,8 @@ class ProducerPipeline:
     def link_elements(self) -> None:
         self.src.link(self.caps_src)
         self.caps_src.link(self.videoconvert)
-        self.videoconvert.link(self.queue)
-        self.queue.link(self.vp8enc)
-        self.vp8enc.link(self.rtppay)
-        self.rtppay.link(self.caps_rtp)
-        self.caps_rtp.link(self.sink)
+        self.videoconvert.link(self.caps_rgba)
+        self.caps_rgba.link(self.sink)
         self.sink.connect("new-sample", self.on_new_sample)
 
     def play(self) -> None:
@@ -132,17 +111,16 @@ class ProducerPipeline:
         if not sample:
             return Gst.FlowReturn.ERROR
         buf = sample.get_buffer()
-        # ok, mapinfo = buf.map(Gst.MapFlags.READ)
-        # if ok:
-        # frame = rtc.VideoFrame(
-        #     width=FRAME_WIDTH,
-        #     height=FRAME_HEIGHT,
-        #     type=rtc.VideoBufferType.RGBA,
-        #     data=bytes(mapinfo.data),
-        # )
-        # self.video_source.capture_frame(frame)
-        # buf.unmap(mapinfo)
-        log.info(f"PTS: {buf.pts // Gst.MSECOND}ms  Size: {buf.get_size()} bytes")
+        ok, mapinfo = buf.map(Gst.MapFlags.READ)
+        if ok:
+            frame = rtc.VideoFrame(
+                width=FRAME_WIDTH,
+                height=FRAME_HEIGHT,
+                type=rtc.VideoBufferType.RGBA,
+                data=bytes(mapinfo.data),
+            )
+            self.video_source.capture_frame(frame)
+            buf.unmap(mapinfo)
         return Gst.FlowReturn.OK
 
     def stop(self) -> None:
