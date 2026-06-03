68db24e0d8b42762d9db583e439e5f72d4874287
Daniel Koch <daniel@dkprog.com>
2026-06-03 18:02:21 +0000

    Consume videos

--- web/src/features/consumer/producer-grid.tsx
+++ web/src/features/consumer/producer-grid.tsx
@@ -6,24 +6,6 @@ import { useRemoteParticipants } from '@livekit/components-react';
 import { useMemo } from 'react';
 import { ProducerTile } from './producer-tile';
 
-/*import { ProducerCount } from './producer-count';
-import { ProducerTile } from './producer-tile';
-import { useWebRTCConsumer } from './use-webrtc-consumer';
-
-export function ProducerGrid() {
-  const producers = useWebRTCConsumer();
-  return (
-    <>
-      <ProducerCount count={producers.length} />
-      <div className="flex flex-wrap gap-4">
-        {producers.map((producer) => (
-          <ProducerTile producer={producer} key={producer.producerId} />
-        ))}
-      </div>
-    </>
-  );
-}*/
-
 export function ProducerGrid() {
   const { room } = useRoom();
   const participants = useRemoteParticipants({ room });
@@ -39,9 +21,9 @@ export function ProducerGrid() {
     <>
       <ProducerCount count={totalProducers} />
       <div className="flex flex-wrap gap-4">
-        {/* {producers.map((producer) => (
-          <br />
-        ))} */}
+        {producers.map((producer) => (
+          <ProducerTile producer={producer} key={producer.sid} />
+        ))}
       </div>
     </>
   );
--- web/src/features/consumer/producer-tile.tsx
+++ web/src/features/consumer/producer-tile.tsx
@@ -1,10 +1,39 @@
+import { useRoom } from '@/lib/livekit/room-context';
 import { VideoTile } from '@/ui/video-tile';
-import { ProducerEntry } from './use-webrtc-consumer';
+import { TrackReference, useTracks } from '@livekit/components-react';
+import { RemoteParticipant, Track } from 'livekit-client';
+import { useMemo } from 'react';
 
 interface ProducerTileProps {
-  producer: ProducerEntry;
+  producer: RemoteParticipant;
+}
+
+function deriveStatus(ref?: TrackReference) {
+  const pub = ref?.publication;
+
+  if (!pub) {
+    return 'idle';
+  }
+  if (!pub.isSubscribed || !pub.track) {
+    return 'loading';
+  }
+  return 'connected';
 }
 
 export function ProducerTile({ producer }: ProducerTileProps) {
-  return <VideoTile id={producer.producerId} status={producer.status} stream={producer.stream} />;
+  const { room } = useRoom();
+  const tracks = useTracks([Track.Source.Camera], { room });
+  const producerTrack = useMemo(
+    () => tracks.find((ref) => ref.participant.sid === producer.sid),
+    [tracks, producer.sid],
+  );
+  const status = useMemo(() => deriveStatus(producerTrack), [producerTrack]);
+
+  return (
+    <VideoTile
+      id={producer.sid}
+      status={status}
+      stream={producerTrack?.publication.track?.mediaStream}
+    />
+  );
 }
